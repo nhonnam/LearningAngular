b2dd5384d1cf9a0b056feacf073378de84ef4056
Cậu Bé Vuôi Vẻ <92206104+nhonnam@users.noreply.github.com>
2023-03-26 23:40:23 +0700

    angular directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,52 @@ import { ChangeDetectorRef } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  // postArray: Array<string> = ['Post 1', 'Post 2', 'Post 3', 'Post 4', 'Post 5'];
 
+  // objArray: Array<any> = [
+    // {id: 1, postTitle: 'Post 1'},
+    // {id: 2, postTitle: 'Post 2'},
+    // {id: 3, postTitle: 'Post 3'},
+    // {id: 4, postTitle: 'Post 4'},
+    // {id: 5, postTitle: 'Post 5'},
+  // ];
+
+  // stepForm: string = 'Something Else';
+
+  // isActive: boolean = true;
+
+  constructor(){
+    
+  }
+
+  // addNew(){
+  //   this.objArray.push({id: 6, postTitle: 'post 6'})
+  // }
+
+  // onDelete(index: any){
+  //   this.objArray.splice(index, 1);
+  // }
+
+  // onClick(status: string){
+  //     this.stepForm = status;
+  // }
+
+  name: string;
+  email: string;
+  address: string;
+
+  userArray: Array<any>=[];
+
+  onClick(){
+    this.userArray.push({
+      "name": this.name,
+      "email": this.email,
+      "address": this.address
+    })
+    console.log(this.userArray);  
+  }
+
+  onDelete(index: number){
+    this.userArray.splice(index, 1);
+  }
 }
