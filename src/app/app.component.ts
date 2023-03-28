5d38f339e8c95f8ea50b60322248babc60cdc5df
nam <nobeltia@gmail.com>
2023-03-28 13:51:24 +0700

    angular template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
-import { ChangeDetectorRef } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
 
+  // form!: FormGroup
+  // constructor(
+  //   private fb: FormBuilder
+  // ) {}
+
+  onSubmit(f: NgForm) {
+    console.log(f);
+  }
+
+  getValue(f: any) {
+    console.log(f);
+  }
+
+  // build() {
+  //   this.form = this.fb.group({
+  //       fname: [null, [Validators.required, Validators.minLength]]
+  //   })
+  // }
 }
