51f3184ee51d583dada8a25d401b24078f1b5406
Cậu Bé Vuôi Vẻ <92206104+nhonnam@users.noreply.github.com>
2023-03-28 09:18:39 +0700

    reactive custom form validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ChangeDetectorRef } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { noSpace } from './validators/nospace.validators';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,21 @@ import { ChangeDetectorRef } from '@angular/core';
 })
 export class AppComponent {
 
+  form: any;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      username: ['', [
+        Validators.required,
+        Validators.minLength(5),
+        noSpace.noSpaceValidations
+      ]],
+
+      password: ['', Validators.required]
+    })
+  }
+
+  get fc() {
+    return this.form.controls;
+  }
 }
